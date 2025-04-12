d9365e1f81212f7a8bbf5668ffb3fda882eedfcd
Yurii <dmukhailo@gmail.com>
2025-04-12 09:20:34 +0300

    upd CamelCase

--- homework_tests_8/hw08_test.py
+++ homework_tests_8/hw08_test.py
@@ -2,7 +2,7 @@ import unittest
 from hw07.homework_07 import  make_summ, average_num, list_with_str, counting_unique
 
 
-class maker_sum_test(unittest.TestCase):
+class MakerSumTest(unittest.TestCase):
     """
     Тестуємо функцію make_summ
     """
@@ -17,7 +17,7 @@ class maker_sum_test(unittest.TestCase):
     def test_with_float_second(self):
         self.assertEqual(make_summ(2.05, 3.04), 5.09)
 
-class average_num_test(unittest.TestCase):
+class AverageNumTest(unittest.TestCase):
     """
     Тестуємо функцію average_num - середнє значення
     """
@@ -27,7 +27,7 @@ class average_num_test(unittest.TestCase):
         self.assertEqual(average_num(2.5, 3.5, 4, 4.05), 3.5125)
     def test_if_type_error(self):
         self.assertRaises(TypeError, average_num, "2", 3)
-class list_with_str_test(unittest.TestCase):
+class ListWithStrTest(unittest.TestCase):
     """
     Тестуємо функцію яка фільтрує список і повертає значення з типом str
     """
@@ -37,7 +37,7 @@ class list_with_str_test(unittest.TestCase):
         self.assertEqual(list_with_str([None, "2", "3"]), ["2", "3"])
         self.assertEqual(list_with_str(["", "2", "3"]), ["", "2", "3"])
 
-class counting_unique_test(unittest.TestCase):
+class CountingUniqueTest(unittest.TestCase):
     def test_if_equal(self):
         self.assertEqual(counting_unique([1, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 11]), True)
         self.assertEqual(counting_unique(["h", "h", "h", "h", "h", "h", "h"]), False)
