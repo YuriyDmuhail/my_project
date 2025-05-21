395176fe50e178c861c54bf9be1726fab1d35e03
Yurii <dmukhailo@gmail.com>
2025-05-21 20:01:37 +0300

    upd 16.1

--- hw16/hw16_1/hw16_1.py
+++ hw16/hw16_1/hw16_1.py
@@ -32,11 +32,3 @@ class TeamLead(Manager, Developer):
 
 #lead = TeamLead("Sara", 25, "IT", "Python", 3)
 
-def test_attributes():
-    lead = TeamLead("Oksana", 5000, "IT", "Python", 4)
-
-    assert hasattr(lead, "name")
-    assert hasattr(lead, "salary")
-    assert hasattr(lead, "department")
-    assert hasattr(lead, "programming_language")
-    assert hasattr(lead, "team_size")
